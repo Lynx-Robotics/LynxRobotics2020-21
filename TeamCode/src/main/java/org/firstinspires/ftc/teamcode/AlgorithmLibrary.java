921fe90820937ef961510bd9631dedf6afe2eddf
Carter <35508836+Cvhelp@users.noreply.github.com>
2020-08-10 14:50:24 -0500

    Revert "Save"
    This reverts commit a585d58d0e3c9ffd588790ecc9c2fb366c76f89c.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlgorithmLibrary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlgorithmLibrary.java
@@ -1,18 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-import java.util.Locale;
-
 public class AlgorithmLibrary {
-    TypexChart chart = new TypexChart();
 
-    //Average Value
     double values[] = new double[10];
     int indexValue = 0;
     double average;
@@ -22,24 +11,11 @@ public class AlgorithmLibrary {
         for (int k = 0; k < values.length; k++) {
             total += values[k];
         }
-        average = total / 10;
+        average = total/10;
         values[indexValue] = value;
         indexValue++;
         if (indexValue > 9) indexValue = 0;
 
-        return average;
+        return  average;
     }
-
-    //IMU Values
-    public Orientation angles;
-    public Acceleration gravity;
-    public double heading;
-    public double roll;
-    public double pitch;
-    public double accX;
-    public double accY;
-    public double accZ;
-
-    public int i = 0;
 }
-
