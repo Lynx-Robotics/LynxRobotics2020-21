9846f26cd7b18c0de327882fbdea9f370a1db805
Carter <35508836+Cvhelp@users.noreply.github.com>
2020-08-13 10:57:16 -0500

    Working Kalman filter
    Filter needs to be updated to work with robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GaussianLibrary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GaussianLibrary.java
@@ -1,11 +1,9 @@
-package org.firstinspires.ftc.teamcode;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GaussianLibrary {
-    GaussianLibrary lib = new GaussianLibrary();
     public double[] gaussian(double mean, double variance) {
-        double gaussian[] = new double[2];
-        gaussian[0] = mean;
-        gaussian[1] = variance;
+        double gaussian[] = new double[] {mean, variance};
 
         return gaussian;
     }
@@ -19,17 +17,40 @@ public class GaussianLibrary {
     }
 
     public double[] predict(double[] pos, double[] movement) {
-        return gaussian(lib.mean(pos) + lib.mean(movement), lib.variance(pos) + lib.variance(movement));
+        System.out.println();
+        return gaussian((mean(pos) + mean(movement)), (variance(pos) + variance(movement)));
     }
 
     public double[] gaussian_multiply(double[] g1, double[] g2) {
-        double mean = (lib.variance(g1)*lib.mean(g2) + lib.variance(g2)* lib.mean(g1)) / (lib.variance(g1) + lib.variance(g2));
-        double variance = (lib.variance(g1) * lib.variance(g2)) / (lib.variance(g1) + lib.variance(g2));
-        return lib.gaussian(mean, variance);
+        double mean = (variance(g1)*mean(g2) + variance(g2)* mean(g1)) / (variance(g1) + variance(g2));
+        double variance = (variance(g1) * variance(g2)) / (variance(g1) + variance(g2));
+        return gaussian(mean, variance);
     }
 
     public double[] update(double[] prior, double[] likelihood) {
-        double[] posterior = lib.gaussian_multiply(likelihood, prior);
+        double[] posterior = gaussian_multiply(likelihood, prior);
         return posterior;
+    }
+
+    public static void main(String[] args) {
+        GaussianLibrary lib = new GaussianLibrary();
+
+        double process_var = 1; //how much error in the process model
+        double sensor_var = 2;  //the amount of variance in each sensor measurement
+
+        double[] x = lib.gaussian(0, Math.pow(20, 2)); //Starting pos in gaussian
+        int velocity = 1;
+        double dt = 1;
+        double[] process_model = lib.gaussian(velocity*dt, process_var); //How I think the robot is moving
+
+        double[] zs = {1.3535959735108178, 1.8820653967131618, 4.341047429453569, 7.156332673205118, 6.938695089418526, 6.843912342028484, 9.846824080052299, 12.553482049375292, 16.2730841073834, 14.800411177015299};
+
+        for (int i = 1; i < zs.length; i++) {
+            double[] prior = lib.predict(x, process_model);
+            double[] likelihood = lib.gaussian(zs[i], sensor_var);
+            x = lib.update(prior, likelihood);
+            System.out.println("Predict: " + Arrays.toString(prior));
+            System.out.println("Update:  " + Arrays.toString(x));
+        }
     }
 }
